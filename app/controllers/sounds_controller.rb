97862b481e759883ec99fa00c86a0258a5cdfa01
keisuke-sakuma <sakumalittle@yahoo.co.jp>
2020-06-18 06:39:01 +0900

    詳細画面へユーザのidを表示

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -14,6 +14,7 @@ class SoundsController < ApplicationController
   def create
     @sound =Sound.new(sound_params)
     
+    #保存が成功した場合は、@soundで格納されたインスタンスの詳細画面に飛ぶ
     if @sound.save
       flash[:success] = "投稿完了しました！"
       redirect_to @sound
@@ -34,6 +35,7 @@ class SoundsController < ApplicationController
   end
 end
 
+#Soundモデルクラス以外では使用できないことを明示的にしている。
 private
 
 #ストロングパラメータ
