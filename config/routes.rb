f739acd5fbc9a9228bc0ed6497ca22e4a8a5f9c2
keisuke-sakuma <sakumalittle@yahoo.co.jp>
2020-06-17 17:39:25 +0900

    Soundモデルのルーティングの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#SoundモデルのCRUD操作
+get 'sounds', to: 'sounds#index'
+get 'sounds/:id', to: 'sounds#show'
+get 'sounds/new', to: 'sounds#new'
+post 'sounds', to: 'sounds#create'
+put 'sounds/:id', to: 'sounds#update'
+get 'sounds/:id/edit', to: 'sounds#edit'
+delete 'sounds/:id', to: 'sounds#delete'
+
+
+
 end
