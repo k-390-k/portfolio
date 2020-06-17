847bc4f97f8f3e69be1b5a1ed4b1ae92fa12e656
keisuke-sakuma <sakumalittle@yahoo.co.jp>
2020-06-18 08:07:08 +0900

    soundモデルのルーティングをresourcesに書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-
-#SoundモデルのCRUD操作
-get 'sounds', to: 'sounds#index'
-get 'sounds/:id', to: 'sounds#show'
-get 'sounds/new', to: 'sounds#new'
-post 'sounds', to: 'sounds#create'
-put 'sounds/:id', to: 'sounds#update'
-get 'sounds/:id/edit', to: 'sounds#edit'
-delete 'sounds/:id', to: 'sounds#delete'
-
-
-
+resources :sounds
 end
