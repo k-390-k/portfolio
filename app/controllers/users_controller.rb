688fdba7b8c16568d52a193caf6ee6c2434b838e
keisuke-sakuma <sakumalittle@yahoo.co.jp>
2020-06-23 07:03:03 +0900

    ルーティングの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url
+    else render :new
+    
+    end
+
   end
 
   def edit
@@ -10,4 +22,10 @@ class UsersController < ApplicationController
 
   def index
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
